4bead3953d78a16a0711527662e98afdc9d7a357
Masum Hasan <80238860+JaoharRaihan@users.noreply.github.com>
2023-02-05 18:25:15 +0600

    a

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,HttpResponse
 def index(request):
+    context = {
+        "variable":"This is sent"
+     }
     return render(request,'index.html')
     # return HttpResponse("This is homepage")
 def about(request):
