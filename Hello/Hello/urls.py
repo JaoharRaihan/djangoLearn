99570f1d6c245121e073b183e51b95e3f7d07601
Masum Hasan <80238860+JaoharRaihan@users.noreply.github.com>
2023-02-05 20:38:50 +0600

    s

--- Hello/Hello/urls.py
+++ Hello/Hello/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+admin.site.site_header = "Jaohar Raihan Admin"
+admin.site.site_title = "Jaohar Raihan Admin Portal"
+admin.site.index_title = "Welcome to Jaohar Raihan website..."
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls'))
+    
 ]
